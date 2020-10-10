2e64f83a7aa9ebd474ee9ddbd137f69a3ee6e9ac
alain@twelvepics <info@twelvepics.com>
2020-10-10 16:54:35 +0800

    Fixed bad routes

--- db/subscribersdb.js
+++ db/subscribersdb.js
@@ -1,141 +1,162 @@
 const arango = require("arangojs");
 const aql = arango.aql;
-const arango_connection = require('./arangodb');
-const { IntegrityError } = require('../utils/errors');
+const arango_connection = require("./arangodb");
+const { IntegrityError } = require("../utils/errors");
 
 /***********************************************************************
  * SUBSCRIBERS
  ***********************************************************************/
 
 /* get subscription stats, numbers errors etc */
-const getStats = async () => {
-
-}
+const getStats = async () => {};
 /* create subscriber */
 const saveSubscriber = async (payload) => {
-    const db = arango_connection.getDb()
-    const collection = db.collection('subscribers');
-    // build it
-    console.log('-----');
-    console.log(payload);
-    console.log('-----');
-    _subscriber = { ...payload }
-    _subscriber.is_in = true;
-    _subscriber.date_created = new Date();
-    _subscriber.email_confirmed = false;
-    _subscriber.date_removed = null;
-    // and save
-    const subscriber = await collection.save(_subscriber, { returnNew: true });
-    return subscriber.new;
-}
+  const db = arango_connection.getDb();
+  const collection = db.collection("subscribers");
+  // build it
+  console.log("-----");
+  console.log(payload);
+  console.log("-----");
+  _subscriber = { ...payload };
+  _subscriber.is_in = true;
+  _subscriber.date_created = new Date();
+  _subscriber.email_confirmed = false;
+  _subscriber.date_removed = null;
+  // and save
+  const subscriber = await collection.save(_subscriber, { returnNew: true });
+  return subscriber.new;
+};
 
 /* getsubscriber by key */
 const getSubscriberBykey = async (key) => {
-    const db = arango_connection.getDb()
-    const collection = db.collection('subscribers');
-    const q = aql`
+  const db = arango_connection.getDb();
+  const collection = db.collection("subscribers");
+  const q = aql`
             FOR s IN ${collection}
             FILTER s._key == ${key} && s.is_in == true
             RETURN s
-        `
-    const cursor = await db.query(q)
-    const user = await cursor.next()
-    return user
-}
+        `;
+  const cursor = await db.query(q);
+  const user = await cursor.next();
+  return user;
+};
 
 /* delete subscriber */
 const deleteSubscriber = async (subscriberKey) => {
-    const db = arango_connection.getDb();
-    const collection = db.collection('subscribers');
-    const done = await collection.update(subscriberKey, { is_in: false });
-    return !!done;
-}
+  const db = arango_connection.getDb();
+  const collection = db.collection("subscribers");
+  const done = await collection.update(subscriberKey, { is_in: false });
+  return !!done;
+};
 
 const updateSubscriber = async (subscriberKey, kvToUpdate) => {
-    console.log(`++> ${subscriberKey}`)
-    const db = arango_connection.getDb()
-    const collection = db.collection('subscribers');
-    const subscriber = await collection.update(subscriberKey, kvToUpdate, { returnNew: true });
-    return subscriber;
-}
+  console.log(`++> ${subscriberKey}`);
+  const db = arango_connection.getDb();
+  const collection = db.collection("subscribers");
+  const subscriber = await collection.update(subscriberKey, kvToUpdate, {
+    returnNew: true,
+  });
+  return subscriber;
+};
 /***********************************************************************
  * EMAILS
  ***********************************************************************/
 
 /* Does filed already exist in ocllection */
-const fieldToLowerExists = async (fname, fvalue, collName = 'subscribers') => {
+const fieldToLowerExists = async (fname, fvalue, collName = "subscribers") => {
+  const db = arango_connection.getDb();
+  const collection = db.collection(collName);
 
-    const db = arango_connection.getDb()
-    const collection = db.collection(collName);
-
-    const q = aql`
+  const q = aql`
             FOR u IN ${collection}
             FILTER  LOWER(u.${fname}) == ${fvalue.toLowerCase()}
             RETURN u
-        `
-    const cursor = await db.query(q)
-    const found = await cursor.next()
-    if (!found) {
-        return false
-    } else {
-        throw new IntegrityError(`This ${fname} is already registered.`, fname)
-    }
-}
+        `;
+  const cursor = await db.query(q);
+  const found = await cursor.next();
+  if (!found) {
+    return false;
+  } else {
+    throw new IntegrityError(`This ${fname} is already registered.`, fname);
+  }
+};
 
 const emailExistsAndVerified = async (email) => {
-    const db = arango_connection.getDb()
-    const collection = db.collection('subscribers');
-    const q = aql`
+  const db = arango_connection.getDb();
+  const collection = db.collection("subscribers");
+  const q = aql`
             FOR s IN ${collection}
             FILTER  LOWER(s.email) == ${email.toLowerCase()} && s.email_confirmed == true
             RETURN s
-        `
-    const cursor = await db.query(q)
-    const found = await cursor.next()
-    if (!found) {
-        throw new IntegrityError('Email is not in database or not verified')
-    } else {
-        return true
-    }
-}
+        `;
+  const cursor = await db.query(q);
+  const found = await cursor.next();
+  if (!found) {
+    throw new IntegrityError("Email is not in database or not verified");
+  } else {
+    return true;
+  }
+};
+
+const getAllEmailConfirmsKeys = async () => {
+  const db = arango_connection.getDb();
+  const collection = db.collection("email_confirm");
+  const q = aql`
+        FOR v IN ${collection} RETURN v._key
+    `;
+  const cursor = await db.query(q);
+  const all_confirms = await cursor.all();
+  return all_confirms;
+};
 
 const createValidateEmailEntry = async (subscriber_key) => {
-    const db = arango_connection.getDb();
-    const collection = db.collection('email_confirm');
-    const entry = {
-        subscriber_key,
-        done: false,
-        date_created: new Date(),
-        date_updated: null,
-        sent: false,
-        date_sent: null,
-
-    }
-    const saved = await collection.save(entry);
-    return saved;
-}
+  const db = arango_connection.getDb();
+  const collection = db.collection("email_confirm");
+  const entry = {
+    subscriber_key,
+    done: false,
+    date_created: new Date(),
+    date_updated: null,
+    sent: false,
+    date_sent: null,
+  };
+  const saved = await collection.save(entry);
+  return saved;
+};
 
 const getValidateEmailEntry = async (entryKey) => {
-    const db = arango_connection.getDb();
-    const collection = db.collection('email_confirm');
-    const doc = await collection.document(entryKey, true);
-    if (doc === null) {
-        // the document does not exist
-        return undefined
-    }
-    return doc
-}
+  const db = arango_connection.getDb();
+  const collection = db.collection("email_confirm");
+  const doc = await collection.document(entryKey, true);
+  if (doc === null) {
+    // the document does not exist
+    return undefined;
+  }
+  return doc;
+};
 
 const setValidateEmailEntryDone = async (entryKey) => {
-    const db = arango_connection.getDb();
-    const collection = db.collection('email_confirm');
-    const date_updated = new Date();
-    const done = await collection.update(entryKey, { done: true, date_updated }, { returnNew: true });
-}
-
+  const db = arango_connection.getDb();
+  const collection = db.collection("email_confirm");
+  const date_updated = new Date();
+  const done = await collection.update(
+    entryKey,
+    { done: true, date_updated },
+    { returnNew: true }
+  );
+};
 
 module.exports = {
-    getStats, saveSubscriber, getSubscriberBykey, updateSubscriber, deleteSubscriber, fieldToLowerExists,
-    emailExistsAndVerified, emailExistsAndVerified, createValidateEmailEntry, getValidateEmailEntry,
-    setValidateEmailEntryDone
-}
+  getStats,
+  saveSubscriber,
+  getSubscriberBykey,
+  updateSubscriber,
+  deleteSubscriber,
+  fieldToLowerExists,
+  emailExistsAndVerified,
+  emailExistsAndVerified,
+  createValidateEmailEntry,
+  getValidateEmailEntry,
+  setValidateEmailEntryDone,
+  getAllEmailConfirmsKeys,
+};
--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,34 +1,40 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 // LOGGING
-var { api_logger, subscribers_events_logger, event_info } = require('../logger');
-const subscribersdb = require('../db/subscribersdb');
+var {
+  api_logger,
+  subscribers_events_logger,
+  event_info,
+} = require("../logger");
+const subscribersdb = require("../db/subscribersdb");
 const {
   validateSubscriber,
   invalidSubscriberErrorsJSON,
-} = require('../validation/validator');
+} = require("../validation/validator");
 
 const {
   confirmEmailMessage,
   newSubscriberMessage,
-  errorMessage
-} = require('../mailer/messages');
+  errorMessage,
+} = require("../mailer/messages");
 
-const { addMailTask } = require('../tasks');
+const { addMailTask } = require("../tasks");
 
 /* send confirm enail request */
 const sendValidateEmail = async (subscriber) => {
   // goes to collection => 'email_confirm'
-  const validateEmailEntry = await subscribersdb.createValidateEmailEntry(subscriber._key);
+  const validateEmailEntry = await subscribersdb.createValidateEmailEntry(
+    subscriber._key
+  );
   const front_url = process.env.FRONT_URL;
-  const validate_url = `${front_url}/email-confirm/${validateEmailEntry._key}`;
+  const validate_url = `${front_url}/email-confirm?confirmId=${validateEmailEntry._key}`;
   // build message I'm sending
   const message = confirmEmailMessage(subscriber.email, validate_url);
   // console.log(message);
   // send to mail queue
   addMailTask({
-    type: 'confirm',
+    type: "confirm",
     key: validateEmailEntry._key,
     payload: message,
   });
@@ -39,94 +45,110 @@ const sendNewSubscriberEmail = async (subscriber) => {
   const message = newSubscriberMessage(subscriber.email);
   console.log(message);
   addMailTask({
-    type: 'new_subscriber',
+    type: "new_subscriber",
     payload: message,
   });
-  true
+  true;
 };
 
 const sendErrorMessage = async (error) => {
   const message = errorMessage(error);
   console.log(message);
   addMailTask({
-    type: 'error_infor',
+    type: "error_infor",
     payload: message,
   });
-  true
-}
+  true;
+};
 
 const createSubscriber = async (subscriberData, req) => {
   // validate -------------------------------------------------------------
   const validSubscriber = await validateSubscriber(subscriberData);
-  await subscribersdb.fieldToLowerExists('email', validSubscriber.email);
+  await subscribersdb.fieldToLowerExists("email", validSubscriber.email);
   const payload = {
     email: validSubscriber.email,
-    'x-forwarded-for': req.headers['x-forwarded-for'],
-    'x-real-ip': req.headers['x-real-ip']
-  }
+    "x-forwarded-for": req.headers["x-forwarded-for"],
+    "x-real-ip": req.headers["x-real-ip"],
+  };
   // save -----------------------------------------------------------------
   const subscriber = await subscribersdb.saveSubscriber(payload);
   await sendValidateEmail(subscriber);
   await sendNewSubscriberEmail(subscriber);
   // await sendErrorMessage(new Error('YOLO'));
   // done
-  return { status: 'OK', subscriber };
+  return { status: "OK", subscriber };
 };
 
 /* TODO */
 /* GET subscribers stats */
-router.get('/', function (req, res, next) {
+router.get("/", function (req, res, next) {
   try {
     subscribers_events_logger.info(`Get subscribers stats\n${event_info(req)}`);
-    res.status(200).send({ go: 'gogo' });
+    res.status(200).send({ go: "gogo" });
   } catch (error) {
     api_logger.error(error.stack.toString());
-    res.status(500).send({ error: 'SERVER ERROR' });
+    res.status(500).send({ error: "SERVER ERROR" });
   }
 });
 
 /* Create new subscriber */
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const subscriberData = req.body;
     const { subscriber } = await createSubscriber(subscriberData, req);
-    subscribers_events_logger.info(`create subscriber\n${event_info(req, subscriber)}`);
-    return res.status(201).send({ status: 'OK' });
+    subscribers_events_logger.info(
+      `create subscriber\n${event_info(req, subscriber)}`
+    );
+    return res.status(201).send({ status: "OK" });
   } catch (e) {
     // api_logger.error(error.stack.toString());
     // res.status(500).send({ error: 'SERVER ERROR' });
-    if (e.name === 'ValidationError') {
+    if (e.name === "ValidationError") {
       api_logger.debug(e.stack.toString());
       const errors = invalidSubscriberErrorsJSON(e);
       return res
         .status(400)
-        .send({ error_type: 'INVALID_EMAIL_ERROR', errors: errors });
-    } else if (e.name === 'IntegrityError') {
+        .send({ error_type: "INVALID_EMAIL_ERROR", errors: errors });
+    } else if (e.name === "IntegrityError") {
       api_logger.debug(e.stack.toString());
       const error = e.error;
       return res
         .status(400)
-        .send({ error_type: 'INTEGRITY_ERROR', errors: error });
+        .send({ error_type: "INTEGRITY_ERROR", errors: error });
     } else {
       api_logger.error(e.stack.toString());
       await sendErrorMessage(e);
       return res
         .status(500)
-        .send({ error_type: 'SERVER_ERROR', errors: 'SERVER ERROR' });
+        .send({ error_type: "SERVER_ERROR", errors: "SERVER ERROR" });
     }
   }
 });
 
+// router.get("/validate-emails", async (req, res) => {
+//   try {
+//     const toValidateKeys = await subscribersdb.getAllEmailConfirmsKeys();
+//     console.log(toValidateKeys);
+//     return res.send(toValidateKeys);
+//   } catch (e) {
+//     api_logger.error(e.stack.toString());
+//     await sendErrorMessage(e);
+//     return res
+//       .status(500)
+//       .send({ error_type: "SERVER_ERROR", errors: "SERVER ERROR" });
+//   }
+// });
+
 /* TODO */
 /* get subscriber by uuid */
-router.get('/:key', async (req, res) => {
-  //
-});
+// router.get("/:key", async (req, res) => {
+//   //
+// });
 
 /*
  * validate email page
  */
-router.post('/validate-email/:validate_key', async (req, res) => {
+router.post("/validate-email/:validate_key", async (req, res) => {
   try {
     // simulate net latency
     // const value = await new Promise((resolve) => setTimeout(resolve, 3000))
@@ -141,28 +163,31 @@ router.post('/validate-email/:validate_key', async (req, res) => {
           req.user
         )}\nvalidate_key: ${validate_key}`
       );
-      return res.status(404).send({ error: 'NOT FOUND' });
+      return res.status(404).send({ error: "NOT FOUND" });
     } else {
       const confirm_email_updated = await subscribersdb.setValidateEmailEntryDone(
         validate_key
       );
       // update subscriber set email_confirmed to true
-      const subscriber_updated = await subscribersdb.updateSubscriber(entry.subscriber_key, {
-        email_confirmed: true,
-      });
+      const subscriber_updated = await subscribersdb.updateSubscriber(
+        entry.subscriber_key,
+        {
+          email_confirmed: true,
+        }
+      );
       res.send({ done: true });
     }
   } catch (e) {
     api_logger.error(e.stack.toString());
-    process.env.NODE_ENV === 'production' && await sendErrorMessage(e);
-    return res.status(500).send({ error: 'SERVER ERROR' });
+    process.env.NODE_ENV === "production" && (await sendErrorMessage(e));
+    return res.status(500).send({ error: "SERVER ERROR" });
   }
 });
 
-/* TODO 
-* validate resend confirm email
-*/
-router.post('/:key/send-confirm-email', async (req, res) => {
+/* TODO
+ * validate resend confirm email
+ */
+router.post("/:key/send-confirm-email", async (req, res) => {
   // TODO
 });
 
